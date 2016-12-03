8b3763db128e426e4ba801ff5a17338e3662a42f
PedraM <pedramp22@gmail.com>
2016-12-03 12:27:51 +0330

    hi

---
+++ esida.php
@@ -0,0 +1,36 @@
+<style>
+.sidebar { 
+	float:right;
+	width:210px;
+	margin-left:10px;
+}
+.info { 
+	box-shadow:0px 0px 1px #207245;
+	width:100%;
+	background:white;
+	border:2px solid #207245;
+	padding:10px;
+}
+.info p {
+	font-family:btitrbold;
+	text-align:justify;
+	font-size:14px;
+}
+.linkbar { 	
+	box-shadow:0px 0px 1px #207245;
+	width:100%;
+	background:white;
+	border:2px solid #207245;
+	padding:10px;margin:10px 0 10px 10px;
+}
+</style>
+<div class="sidebar">
+		<div class="info">
+            <p>	تیم ما در راستای کمک به آموزش هر چه آسان تر و سریع تر جوانان ایرانی اقدام به طراحی و ساخت این سایت نموده که
+            قصد دارد با در اختیار قرار دادن مطالب حاضر در سایت بصورت رایگان گامی در راستای سهولت یادگیری نرم افزارهای پرکاربرد بردارد.</p>
+		</div>
+		<div class="linkbar">
+       		linkbar
+        </div>
+	</div>
+	
